5ab9fb0a292ad431308d7832bf05e6cc07b96e4b
ToniIvars <toniivaju@gmail.com>
2021-03-23 15:47:52 +0100

    Added comments for the explanation of the server code

--- app.py
+++ app.py
@@ -12,21 +12,30 @@ app.register_error_handler(404, page_not_found)
 def shortener(url):
     with open('urls.json', 'r+') as f:
         digits = 'ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz'
+
+        # Create the shortened URL merging 7 digits of above randomly
         shortened_url = f'http://localhost:5000/{"".join(sample(digits, 7))}'
 
+        # Try-except for controlling if the json file is empty or not
         try:
+            # If json file is not empty, load the data as a dictionary
             data = json.load(f)
             data_values = tuple(data.values())
+
+            # If the url is already in the file, the function will return it
             if url in data_values:
                 return tuple(data.keys())[data_values.index(url)]
 
+            # The data is updated with the key-value pair of the urls
             dictionary = {shortened_url:url}
             data.update(dictionary)
             f.seek(0)
 
         except:
+            # If json file is empty, this will set the data to be only the pair or urls
             data = {shortened_url:url}
 
+        # Set the info in the file as the data we have already defined
         json.dump(data, f, indent=4)
         f.close()
 
@@ -39,6 +48,7 @@ def index():
 @app.route('/acortado', methods=('GET', 'POST'))
 def acortado():
     if request.method == 'POST':
+        # Get the url from the form
         data = request.form['data']
     
         if not data:
@@ -53,12 +63,14 @@ def acortado():
         return render_template('acortado.html', url=url)
         
     else:
+        # Returns an error template if the method is GET
         return render_template('error.html')
 
 @app.route('/<digits>', methods=('GET',))
 def redirect_to_url(digits):
     url = f'http://localhost:5000/{digits}'
     
+    # Tryes to load the data of the json file. It will fail if the file is empty
     try:
         with open('urls.json', 'r') as f:
             dictionary = json.load(f)
@@ -66,6 +78,8 @@ def redirect_to_url(digits):
     except:
         return abort(404)
     
+    # Check if the digits are in the json file. If they are, this will redirect to the url,
+    # else it will return an error
     if url in dictionary.keys():
         redirect_url = dictionary[url]
         return render_template('redirect.html', url=redirect_url)
@@ -75,14 +89,18 @@ def redirect_to_url(digits):
 
 @app.route('/api')
 def api_mode():
+    # Gets the url from the 'url' arg of the url
     url = request.args['url']
 
+    # Return errors if the 'url' arg doesen't exist or is not valid
     if not url:
         return 'You must enter a url as an argument (/api?url=https://www.google.com)'
 
     elif 'http' not in url:
         return 'You must enter a valid url'
 
+    # Shortens the url
     shortened_url = shortener(url)
 
+    # Returns a 'json object' with the original url and the shortened one
     return {'original':url, 'shortened':shortened_url}
